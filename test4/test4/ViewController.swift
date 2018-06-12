eaa9ae13d3af63d8c4a323504035bab3f99af99c
phirenlele <17521020680@163.com>
2018-06-12 13:50:57 +0800

    我是一只鱼

--- test4/test4/ViewController.swift
+++ test4/test4/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print("我是一只鱼");
     }
 
     override func didReceiveMemoryWarning() {
